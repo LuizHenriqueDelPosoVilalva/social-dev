5b6f71294f81155df92316e569913f87a13b3575
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-01-09 16:25:15 -0300

    Instalado e configurado styled components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,22 @@
-import '../style.css'
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle= createGlobalStyle `
+  * {
+    padding: 0px;
+    margin: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 function App ({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
   )
 }
 
