3c1cc8ac2dd4afb23cbead25ee079945c6a475d1
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-01-09 21:43:30 -0300

    criado esqueleto da pagina de login

--- src/components/layout/ImageWithSpace.jsx
+++ src/components/layout/ImageWithSpace.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const WIDTH_BREAK= '600px'
+const WIDTH_BREAK= '700px'
 const StyleFlex= styled.div `
   display: flex;
 `
@@ -20,16 +20,19 @@ const StyleImage= styled.div `
 
 const StyleContainer= styled.div `
   background-color: white;
-  padding: 30px;
+  padding: 30px 50px;
 
   @media (min-width: ${WIDTH_BREAK}) {
-    width: 100%;
-    min-width: calc(${WIDTH_BREAK}- 60);
+    width: calc(${WIDTH_BREAK} - 100px);
   }
 
   @media (max-width: ${WIDTH_BREAK}) {
     width: 100%;
   }
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `
 
 function ImageWithSpace ({children, image}) {
