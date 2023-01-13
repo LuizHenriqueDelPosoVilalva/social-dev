b436cd16270b9d462ae4abaf9d56b20fcb90138e
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-01-13 20:46:11 -0300

    criado middleware para conexão com db

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,5 +1,6 @@
-import connect from 'next-connect'
 import Joi from 'joi'
+
+import createHandler from '../../../lib/middlewares/nextConnect'
 
 import validate from '../../../lib/middlewares/validate'
 
@@ -13,8 +14,9 @@ const postSchema= Joi.object({
   password: Joi.string().required().max(50).min(6),
 })
 
-const signup= connect()
-    .post(validate({ body:postSchema }), (req,res) => {
+const signup= createHandler()
+    
+signup.post(validate({ body:postSchema }), (req,res) => {
       signupUser(req.body)
       res.status(200).json({teste: "ok"})
     })
