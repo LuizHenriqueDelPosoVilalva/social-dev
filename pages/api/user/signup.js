06c8ba595da55f80286571ce53c6533eb773bb75
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-01-16 23:01:56 -0300

    criado model de user

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -16,9 +16,13 @@ const postSchema= Joi.object({
 
 const signup= createHandler()
     
-signup.post(validate({ body:postSchema }), (req,res) => {
-      signupUser(req.body)
-      res.status(200).json({teste: "ok"})
-    })
+signup.post(validate({ body:postSchema }), async(req,res) => {
+  try {
+    const user= await signupUser(req.body)
+    res.status(201).json(user)
+} catch (err) {
+    console.error(err)
+  }
+})
 
 export default signup
